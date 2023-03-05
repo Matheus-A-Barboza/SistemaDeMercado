00886e7dfd5ea9c54c12e0c1a20489d442e6f8b3
Matheus-A-Barboza <106258751+Matheus-A-Barboza@users.noreply.github.com>
2023-03-04 22:51:01 -0300

    Arquivo SQL
    Arquivo SQL que faz conexão com o arquivo "sistema_vendas.py".

---
+++ SQLVendas.sql
@@ -0,0 +1,51 @@
+#Banco de Dados desenvolvido por: @programmerantunes
+#LinkedIn: www.linkedin.com/Matheus-A-Barboza
+#GitHub: www.github.com/Matheus-A-Barboza
+#Feedback's através das redes sociais
+
+create database sistemavendas;
+
+use sistemavendas;
+
+create table alimentos(
+id_alimentos int primary key not null auto_increment,
+nome_alimento varchar(20) not null,
+tipo_produto varchar(9) not null,
+qnt_alimento varchar(20) not null
+);
+
+create table limpeza(
+id_limpeza int primary key not null auto_increment,
+nome_produto varchar(40) not null,
+tipo_produto varchar(7) not null,
+qnt_produto varchar(20) not null
+);
+
+create table higiene(
+id_alimento int primary key not null auto_increment,
+nome_alimento varchar(30) not null,
+tipo_produto varchar(7) not null,
+qnt_alimento varchar(20) not null
+);
+
+create table cadastrarprodutos(
+id_produto int primary key not null auto_increment,
+nome_produto varchar(40) not null,
+tipo_produto varchar(9) not null,
+qnt_produto varchar(20) not null
+);
+
+create table consultaprodutos(
+id_produto int primary key not null auto_increment,
+nome_produto varchar(40) not null,
+qnt_produto varchar(20) not null
+);
+
+create table deposito(
+id_deposito int primary key not null auto_increment,
+nome_produto varchar(40) not null,
+tipo_produto varchar(9) not null,
+qnt_produto varchar(20) not null
+);
+
+#Obrigado por utilizar meu Sistema!
